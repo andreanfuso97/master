4e273ebbb6fe4b4a655301761311a734e298e5f1
andreanfuso97 <123107100+andreanfuso97@users.noreply.github.com>
2023-02-24 17:36:58 +0100

    catalog commit

--- FoodVibes/src/business.java
+++ FoodVibes/src/business.java
@@ -7,4 +7,6 @@ public class business {
 	public business(String name, String surname, String openingHours, String image) {
 		
 	}
+	
+	//prova
 }
--- prova/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author sebyb
- *
- */
-module prova {
-}
