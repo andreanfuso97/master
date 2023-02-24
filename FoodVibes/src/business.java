90cfafb428976d09e991a8fe65887c9052893e3e
sebyb <sebyb@Seby-Desktop.station>
2023-02-24 17:42:53 +0100

    d

--- FoodVibes/src/business.java
+++ FoodVibes/src/business.java
@@ -4,7 +4,7 @@ public class business {
 	String surname;
 	String openingHours;
 	String image;
-	String prova;
+	String prova2;
 	public business(String name, String surname, String openingHours, String image) {
 		
 	}
